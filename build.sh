a881fb99efc5231fb9f9b49bd1c4533d109ed1dd
Jonathan Bouzekri <jonathan.bouzekri@gmail.com>
2021-01-18 12:08:09 +0100

    use official base image

--- build.sh
+++ build.sh
@@ -8,13 +8,7 @@ then
 	exit 1
 fi
 
-SUPERSET_REPOSITORY=https://github.com/apache/incubator-superset.git
 SUPERSET_TAG=$1
-
-git clone -b $SUPERSET_TAG $SUPERSET_REPOSITORY superset
-
-docker build -t 3slab/superset:$SUPERSET_TAG --target=lean superset/
-docker push 3slab/superset:$SUPERSET_TAG
 
 sed "s/##RELEASE##/$SUPERSET_TAG/" Dockerfile-mysql.template > Dockerfile
 docker build -t 3slab/superset:$SUPERSET_TAG-mysql .
